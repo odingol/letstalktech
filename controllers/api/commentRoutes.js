1b5997d1538884700f266e2266e588e90ed51ed3
Lamor Odingo <l2odingo@gmail.com>
2022-04-14 00:13:27 -0700

    added post references to handlebars

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [User],
+        });
+
+    const comments = commentData.map((commentData) => commentData.get({ plain: true }));
+
+    console.log(comments);
+
+    res.render('commentPost', {comments, logged_in: req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+    const body = req.body;
+    const newComment = await Comment.create({...body, user_id: req.session.user_id});
+    res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
