b71e7bb311bc93b7e9a927acbd1a7e7683c3abe1
Lamor Odingo <l2odingo@gmail.com>
2022-04-16 14:11:45 -0700

    debugged time and date fields

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const routes = require('./controllers');
 const path = require('path');
-const helpers = require('./utils/helpers')
-require('dotenv').config();
-
+const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
+const routes = require('./controllers');
+const helpers = require('./utils/helpers')
+// require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-const hbs = exphbs.create({helpers});
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Hello',
@@ -27,6 +23,7 @@ const sess = {
 };
 
 app.use(session(sess));
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
