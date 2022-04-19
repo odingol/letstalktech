f1804e38f87605736e6fbb52cbba3529f58df209
Lamor Odingo <l2odingo@gmail.com>
2022-04-18 18:30:18 -0700

    restyled comment handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     format_date: (date) => {
         // Formats date as the MM/DD/YYYY
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate() + 1}/${new Date(date).getFullYear()}`;
     }
 };
