3a89fc684e569c02c1cfaac09a0c563303f82812
Lamor Odingo <l2odingo@gmail.com>
2022-04-19 22:16:37 -0700

    added new date functionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
-    format_date: (date) => {
+    format_date: () => {
         // Formats date as the MM/DD/YYYY
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate() + 1}/${new Date(date).getFullYear()}`;
-    }
-};
+        const date = new Date();
+        return date.toLocaleDateString();
+    }};
