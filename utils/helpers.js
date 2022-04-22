e5695949db75aa3448d4f9fa4dc05311ab5c4927
Lamor Odingo <l2odingo@gmail.com>
2022-04-21 22:49:57 -0700

    debugged date method

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
 module.exports = {
-    format_date: (date) => {
+    format_date: () => {
         // Formats date as the MM/DD/YYYY
+        const date = new Date();
         return date.toLocaleDateString();
     }};
